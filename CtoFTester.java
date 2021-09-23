013eacb78e13cddeabecae82981e68b2bb9506d9
Jeffrey Jiang <jjiang30@stuy.edu>
2021-09-23 19:01:22 -0400

    finished celsiusToFahrenheit + added and tested method

--- CtoFTester.java
+++ CtoFTester.java
@@ -11,20 +11,35 @@ What should the function return?
         fahrenheitToCelsius - returns celsius
 
 *************************************************/
+
 public class CtoFTester {
 
     public static void main(String[] args) {
+        // Test values - All should be floating point number
+
+        // Celsius to Fahrenheit conversion tests
+        // 0 C = 32 F
+        System.out.println(celsiusToFahrenheit(0));
+        // 45 C = 113 F
+        System.out.println(celsiusToFahrenheit(45));
+        // -22.5 C = -8.5
+        System.out.println(celsiusToFahrenheit(-22.5));
+
 
     }
 
-    // void + no parameter for stub
-    public static void celsiusToFahrenheit() {
-        
+    public static double celsiusToFahrenheit(double celsius) {
+        // formula = (C * 9/5) + 32
+        double fahrenheit = celsius * 9 / 5 + 32;
+
+        return fahrenheit;
     }
 
-    // void + no parameter for stub
-    public static void fahrenheitToCelsius() {
-        
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        // stub
+        double celsius = 0;
+
+        return celsius;
     }
 
 }
