5f0e0b2069deeeaeb2f44ce04bd8fdbea71df03c
Jeffrey Jiang <jjiang30@stuy.edu>
2021-09-23 19:09:23 -0400

    finished fahrenheitToCelsius; added more tests + clarified test results in console

--- CtoFTester.java
+++ CtoFTester.java
@@ -6,7 +6,7 @@ What should the functions have as parameters?
         fahrenheitToCelsius - Double fahrenheit
 
 What should the function return?
-    function should return a double
+    functions should return a double
         celsiusToFahrenheit - returns fahrenheit
         fahrenheitToCelsius - returns celsius
 
@@ -19,12 +19,30 @@ public class CtoFTester {
 
         // Celsius to Fahrenheit conversion tests
         // 0 C = 32 F
-        System.out.println(celsiusToFahrenheit(0));
+        System.out.println("0 C --> " + celsiusToFahrenheit(0) + " F");
         // 45 C = 113 F
-        System.out.println(celsiusToFahrenheit(45));
+        System.out.println("45 C --> " + celsiusToFahrenheit(45) + " F");
         // -22.5 C = -8.5
-        System.out.println(celsiusToFahrenheit(-22.5));
+        System.out.println("-22.5 C --> " + celsiusToFahrenheit(-22.5) + " F");
 
+
+        // Fahrenheit to Celsius conversion tests
+        // 32 F = 0 C
+        System.out.println("32 F --> " + fahrenheitToCelsius(32) + " C");
+        // 113 F = 45 C
+        System.out.println("113 F --> " + fahrenheitToCelsius(113) + " C");
+        // -8.5 F - -22.5 C
+        System.out.println("-8.5 F --> " + fahrenheitToCelsius(-8.5) + " C");
+
+        // function composition test
+
+        System.out.println("Original is 45: " 
+            + fahrenheitToCelsius(celsiusToFahrenheit(45))
+        );
+
+        System.out.println("Original is -67: "
+            + fahrenheitToCelsius(celsiusToFahrenheit(-67))
+        );
 
     }
 
@@ -36,8 +54,8 @@ public class CtoFTester {
     }
 
     public static double fahrenheitToCelsius(double fahrenheit) {
-        // stub
-        double celsius = 0;
+        // formula = (F - 32) * 5 / 9
+        double celsius = (fahrenheit - 32) * 5 / 9;
 
         return celsius;
     }
