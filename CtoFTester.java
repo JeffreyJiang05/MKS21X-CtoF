7096d058e1c41ad4df7e45cf7b5a5146cea1cecf
Jeffrey Jiang <jjiang30@stuy.edu>
2021-09-23 18:49:19 -0400

    Answered questions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,4 +1,16 @@
+/*************************************************
 
+What should the functions have as parameters? 
+    functions should have a single parameter
+        celsiusToFahrenheit - Double celsius
+        fahrenheitToCelsius - Double fahrenheit
+
+What should the function return?
+    function should return a double
+        celsiusToFahrenheit - returns fahrenheit
+        fahrenheitToCelsius - returns celsius
+
+*************************************************/
 public class CtoFTester {
 
     public static void main(String[] args) {
